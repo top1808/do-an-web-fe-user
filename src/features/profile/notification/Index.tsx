9798a389fbdc5d17a8309f6ed0d84fa7ea1d2d8b
thang <vietthang11012002@gmail.com>
2024-01-29 16:47:03 +0700

    Ui notification

--- src/features/profile/notification/Index.tsx
+++ src/features/profile/notification/Index.tsx
@@ -1,52 +1,73 @@
 'use client';
-
+import MButton from '@/components/MButton';
 import MCol from '@/components/MCol';
 import MRow from '@/components/MRow';
+import MTable from '@/components/MTable';
 import MTitle from '@/components/MTitle';
-import React from 'react';
-// interface NoticeProps {
-// 	id: string;
-// 	content: string;
-// 	time: string;
-// 	from: string;
-// }
+import { useAppDispatch, useAppSelector } from '@/redux/hooks';
+import { gettingMoreNotifications, gettingNotifications } from '@/redux/reducers/notificationReducer';
+import { TableProps } from 'antd';
+import dayjs from 'dayjs';
+import React, { useEffect } from 'react';
+interface DataType {
+	_id: string;
+	body: string;
+	createdAt: string;
+}
+const columns: TableProps<DataType>['columns'] = [
+	{
+		title: '#ID',
+		dataIndex: '_id',
+		key: '_id',
+	},
+	{
+		title: 'Nội dung',
+		dataIndex: 'body',
+		key: 'body',
+	},
+	{
+		title: 'Thời gian',
+		dataIndex: 'createdAt',
+		key: 'createdAt',
+		render: (item: string) => (item ? dayjs(item).format('DD/MM/YYYY') : 'Chưa xác định'),
+	},
+];
 const Notice = () => {
+	const { notification } = useAppSelector((state) => state);
+	const disptach = useAppDispatch();
+
+	const onViewMore = () => {
+		disptach(gettingMoreNotifications({ offset: ((notification.pagination?.limit || 0) + (notification.pagination?.offset || 0)).toString(), limit: '10' }));
+	};
+	useEffect(() => {
+		disptach(gettingNotifications({ limit: '10', offset: '0' }));
+	}, [disptach]);
 	return (
 		<div>
 			<MTitle level={3}>Thông báo</MTitle>
-			<MRow
-				align={'middle'}
-				className='py-2 bg-slate-500 text-white'
-			>
-				<MCol
-					className='px-2'
-					span={4}
+			<MTable
+				columns={columns}
+				dataSource={notification.data?.map((notification, index) => ({ ...notification, index: index + 1, key: notification._id }))}
+				pagination={{ hideOnSinglePage: true }}
+			/>
+			{(notification?.pagination?.total || 0) > 10 && notification.data?.length !== notification.pagination?.total && (
+				<MRow
+					gutter={[4, 4]}
+					align='middle'
+					className='bg-white p-2 hover:bg-slate-100 text-center cursor-pointer'
+					onClick={onViewMore}
 				>
-					#ID
-				</MCol>
-				<MCol span={12}>#Nội dung</MCol>
-				<MCol span={4}>#Người đăng</MCol>
-				<MCol span={4}>#Thời gian</MCol>
-			</MRow>
-			{/* {data &&
-				data.map((item) => {
-					return (
-						<>
-							<MRow>
-								<MCol span={4}>{item.id}</MCol>
-								<MCol span={12}>{item.content}</MCol>
-								<MCol span={4}>{item.from}</MCol>
-								<MCol span={4}>{item.time}</MCol>
-							</MRow>
-						</>
-					);
-				})}
-			{!data ||
-				(data.length < 1 && (
-					<MRow justify={'center'}>
-						<MCol>No data available in table</MCol>
-					</MRow>
-				))} */}
+					<MCol span={24}>
+						<MButton
+							type='text'
+							className='text-sm text-blue-600'
+							loading={notification.isLoadingMore}
+						>
+							View more
+						</MButton>
+					</MCol>
+				</MRow>
+			)}
 		</div>
 	);
 };
--- src/models/notificationModel.ts
+++ src/models/notificationModel.ts
@@ -1,4 +1,5 @@
 export interface NotificationModel {
+	createdAt: any;
 	_id?: string;
 	image?: string;
 	title?: string;
