e82bca8104d1760113b484cdcda88cec5cbf634a
thang <vietthang11012002@gmail.com>
2024-04-28 17:23:50 +0700

    fix loading

--- src/features/cart/Index.tsx
+++ src/features/cart/Index.tsx
@@ -3,24 +3,15 @@ import MTitle from '@/components/MTitle';
 import { useEffect } from 'react';
 import TableCartProducts from './components/TableCartProducts';
 import { useAppDispatch, useAppSelector } from '@/redux/hooks';
-import CartEmty from './components/CartEmty';
+
 import { getCartState, gettingCart, setIPCustomer } from '@/redux/reducers/cartReducer';
 import Loading from '@/components/Loading';
 import axios from 'axios';
+import CartEmpty from './components/CartEmty';
 
 const CartPageComponent = () => {
 	const cart = useAppSelector(getCartState);
 	const dispatch = useAppDispatch();
-	// const params = useSearchParams();
-	// const isSuccess = params.get('vnp_ResponseCode') === '00' ? true : false;
-	// if (isSuccess) {
-	// 	const data = localStorage.getItem('tempDataPayement');
-	// 	// tien hanh tao hoa don voi data
-	// 	console.log(data);
-	// } else {
-	// 	// thanh toan that bai
-	// 	localStorage.removeItem('tempDataPayement');
-	// }
 	useEffect(() => {
 		dispatch(gettingCart());
 		axios.get('https://api.ipify.org/').then((res) => dispatch(setIPCustomer(res.data)));
@@ -32,7 +23,7 @@ const CartPageComponent = () => {
 
 			<div className='py-8'>
 				<MTitle level={2}>My cart</MTitle>
-				<div>{cart?.items?.length < 1 ? <CartEmty /> : <TableCartProducts data={cart?.items} />}</div>
+				<div>{cart?.items?.length < 1 ? <CartEmpty /> : <TableCartProducts data={cart?.items} />}</div>
 			</div>
 		</>
 	);
